c34cfe73abce252003ad5ebe6d6bc853ca1642a8
vladisov <vlad079@ya.ru>
2018-01-17 23:07:54 +0300

    deploy features

--- deploy.js
+++ deploy.js
@@ -17,6 +17,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
+  console.log(interface);
   console.log('Contract deployed to ', result.options.address);
 };
 deploy();
